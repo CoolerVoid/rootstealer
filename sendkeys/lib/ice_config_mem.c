414af965b46fed5719d0e5a037a61fdb07f26f3b
Your Name <coolerlair@gmail.com>
2020-10-13 00:10:37 -0300

    improve error handlers in memory

--- sendkeys/lib/ice_config_mem.c
+++ sendkeys/lib/ice_config_mem.c
@@ -6,14 +6,16 @@ void *ice_config_xmallocarray (size_t nmemb, size_t size)
 	if ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) && nmemb > 0 && SIZE_MAX / nmemb < size) 
 	{
 		ICE_CONFIG_DEBUG("integer overflow block");
-		return NULL;
+		exit(0);
 	}
 
 	void *ptr = malloc (nmemb*size);
 
-	if (ptr == NULL) 
-		return NULL;
-
+	if (ptr == NULL)
+	{	
+		ICE_CONFIG_DEBUG("Size error");
+		exit(0);
+	}
 	return ptr;
 }
 
@@ -23,25 +25,27 @@ void *ice_config_xreallocarray (void *ptr, size_t nmemb, size_t size)
 	if ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) && nmemb > 0 && SIZE_MAX / nmemb < size) 
 	{
 		ICE_CONFIG_DEBUG("integer overflow block");
-		return NULL;
+		exit(0);
 	}
 
 	void *p = realloc (ptr, nmemb*size);
 
 	if (p == NULL) 
-		return NULL;
+	{
+
+		ICE_CONFIG_DEBUG("Size error");
+		exit(0);
+	}
 
 	return p;
 }
 
 void *ice_config_xmalloc_fatal(size_t size) 
 {
-	if ( size == 0 ) 
-		return NULL;
 
-	ICE_CONFIG_DEBUG("\n Memory FAILURE...\n");
+	ICE_CONFIG_DEBUG("\n Memory FAILURE...\n size dbg: %lu\n",size);
 
-	exit(1);
+	exit(0);
 }
 
 void *ice_config_xmalloc (size_t size) 
